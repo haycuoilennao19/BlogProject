44f12dd24011299baba4c7ccca1378ccb51f3031
haycuoilennao19 <haycuoilennao3@gmail.com>
2019-11-29 13:59:53 +0700

    iam do it

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'ketboard cat', cookie: { maxAge: 600000 }, resave: false, saveUninitialized: false}));
+app.use(session({secret: 'ketboard cat', cookie: { maxAge: 6000000 }, resave: false, saveUninitialized: false}));
 app.use(flash())
 app.use(passport.initialize());
 app.use(passport.session())
